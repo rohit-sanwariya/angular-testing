b2889752b142462a531ba55fd90a5c2b9e1afaee
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-01-16 13:49:17 +0530

    moved to calculator folder for better organization

--- src/app/Services/Calculator/calculator.service.spec.ts
+++ src/app/Services/Calculator/calculator.service.spec.ts
@@ -1,14 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CalculatorService } from './calculator.service';
+
 
 describe('CalculatorService', () => {
   let service: CalculatorService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(CalculatorService);
-  });
+    service = TestBed.inject(CalculatorService); 
+  }); 
 
   it('should be created', () => {
     expect(service).toBeTruthy();
