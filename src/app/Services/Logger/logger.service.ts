499ec670477c8c30e009f96bb68149b30055548d
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-01-16 13:52:00 +0530

    new logger service with log method

---
+++ src/app/Services/Logger/logger.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggerService {
+ 
+
+  constructor() { }
+
+
+  log(message: string) {
+     console.timeLog(message);
+  }
+}
