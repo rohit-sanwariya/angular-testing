175e1ed7bdaba815671e6b62499b6cdc08b79e92
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-01-16 13:52:12 +0530

    log message on add

--- src/app/Services/Calculator/calculator.service.ts
+++ src/app/Services/Calculator/calculator.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+import { LoggerService } from '../Logger/logger.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculatorService {
 
-  constructor() { }
+  constructor(
+    private _logger:LoggerService,
+  ) { }
 
   add(x:number,y:number):number{
+    this._logger.log(`Adding ${x} and ${y}`);
     return x+y;
   }
 }
